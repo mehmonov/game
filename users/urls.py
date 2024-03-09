a1b54abecf17307bb72ac874723d69256b123aa3
Husniddin <uzbekfreedom@gmail.com>
2024-03-09 10:06:52 +0500

    add requirements

--- users/urls.py
+++ users/urls.py
@@ -6,8 +6,6 @@ from .api import RegisterApi, LoginApi
 urlpatterns = [
     path('api/token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
-    path('api/register', RegisterApi.as_view()),
+    path('api/register', RegisterApi.as_view(), name='register'),
     path('api/login/', LoginApi.as_view(), name='login'),
-
-    
 ]
