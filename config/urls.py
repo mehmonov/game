be9f093732346aaa436651a6a3534761f940e90a
Husniddin <103229084+mehmonov@users.noreply.github.com>
2024-03-10 19:02:00 +0500

    Update urls.py

--- config/urls.py
+++ config/urls.py
@@ -10,4 +10,4 @@ urlpatterns = [
     path('user/', include("users.urls")),
     path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
